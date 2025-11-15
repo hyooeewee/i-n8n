8604a1a6082102082356218d5594d5cf4dc780ac
hyooeewee <hughie-ngai@outlook.com>
2025-11-15 09:48:41 +0800

    feat: Setup react-flow for workflow canvas

---
+++ src/components/add-node-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { memo } from "react";
+
+export const AddNodeButton = memo(() => {
+  return (
+    <Button
+      className="bg-background"
+      onClick={() => {}}
+      size="icon"
+      variant="outline"
+    >
+      <PlusIcon />
+    </Button>
+  );
+});
+
+AddNodeButton.displayName = "AddNodeButton";
